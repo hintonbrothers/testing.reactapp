7dc7ef847f2eb6faaed6fb38c4d25c2dc50c087c
unknown <hintonbrothersprivatelimited@gmail.com>
2024-07-13 11:13:54 +0530

    testing app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
     return (<div className="App">
         <header className="App-header">
-            <h2>Testing Automation ReactApp From Github To The EC2.......</h2>
+            <h2>Testing Automation ReactApp From Github To The EC2.....</h2>
         </header>
     </div>);
 }
