456462a012fbd5cc1fae2e35a9c2052d36c91c37
unknown <hintonbrothersprivatelimited@gmail.com>
2024-07-13 11:32:57 +0530

    testing ratio

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
     return (<div className="App">
         <header className="App-header">
             <h2>Testing Automation ReactApp From Github To The EC2...</h2>
+            <h2>ReactApp</h2>
         </header>
     </div>);
 }
