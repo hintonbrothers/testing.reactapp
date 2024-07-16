5e2ec6a12dd99179a6854cab76a74b33f6c79fee
unknown <hintonbrothersprivatelimited@gmail.com>
2024-07-16 16:03:03 +0530

    commiting testapp

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
     return (<div className="App">
         <header className="App-header">
             <h2>Testing Automation ReactApp From Github To The EC2...</h2>
-            <h2>CI/CD Automation For Testing</h2>
+            <h2>CI/CD Automation For Testing...</h2>
         </header>
     </div>);
 }
